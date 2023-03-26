95bc2755e9d6e412701db5e5d550f03849e05655
HumanWeapon <odair.umanzor@unah.hn>
2023-03-26 12:31:54 -0600

    aasas

--- routes/router.js
+++ routes/router.js
@@ -1,21 +1,18 @@
 "use strict"
 //ESTO ES UN EJEMPLO DE LAS RUTAS
 var BancoController = require("../controllers/banco.controller",),
-    ClienteController = require("../controllers/cliente.controller"),
-    CuentaController = require("../controllers/cuenta.controller"),
+    express = require("express"),
+    router = express.Router();
 
+var ClienteController = require("../controllers/cliente.controller"),
+    express = require("express"),
+    router = express.Router();
+
+var CuentaController = require("../controllers/cuenta.controller"),
     express = require("express"),
     router = express.Router();
 
 router
-    //****BANCO RUTA****
-    .get("/banco/getall", BancoController.getAll)
-    .post("/banco/getone/:cod_banco", BancoController.getOne)
-    .post("/banco/insertar", BancoController.post)
-    .put("/banco/actualizar/:cod_banco", BancoController.put)
-    .delete("/banco/eliminar/:cod_banco", BancoController.delete)
-    .use(BancoController.error404)
-
 
     //***CLIENTE RUTA***//*
     .get("/cliente/getall", ClienteController.getAll)
@@ -23,8 +20,15 @@ router
     .post("/cliente/insertar", ClienteController.post)
     .put("/cliente/actualizar/:numero_cliente", ClienteController.put)
     .delete("/cliente/eliminar/:numero_cliente", ClienteController.delete)
-    .use(ClienteController.error404)
+    
 
+    //****BANCO RUTA****
+    .get("/banco/getall", BancoController.getAll)
+    .post("/banco/getone/:cod_banco", BancoController.getOne)
+    .post("/banco/insertar", BancoController.post)
+    .put("/banco/actualizar/:cod_banco", BancoController.put)
+    .delete("/banco/eliminar/:cod_banco", BancoController.delete)
+    
 
     //***CUENTA RUTA***//*
     .get("/cuenta/getall", CuentaController.getAll)
@@ -32,6 +36,10 @@ router
     .post("/cuenta/insertar", CuentaController.post)
     .put("/cuenta/actualizar/:numero_cuenta", CuentaController.put)
     .delete("/cuenta/eliminar/:numero_cuenta", CuentaController.delete)
+
+    
+    .use(ClienteController.error404)
     .use(CuentaController.error404)
+    .use(BancoController.error404)
 
 module.exports = router;
