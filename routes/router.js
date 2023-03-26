31b49c7aec0cbdff9f1c1be5ad912088f647c796
HumanWeapon <odair.umanzor@unah.hn>
2023-03-26 01:36:25 -0600

    prueba odair

--- routes/router.js
+++ routes/router.js
@@ -1,32 +1,42 @@
-"use strict";
+"use strict"
 //ESTO ES UN EJEMPLO DE LAS RUTAS
 var BancoController = require("../controllers/banco.controller"),
-    ClienteController = require("../controllers/cliente.controller"),
-    CuentaController = require("../controllers/cuenta.controller"),
     express = require("express"),
     router = express.Router();
 
 router
-  //****BANCO RUTA****
+    //****BANCO RUTA****
     .get("/banco/getall", BancoController.getAll)
     .post("/banco/getone/:cod_banco", BancoController.getOne)
     .post("/banco/insertar", BancoController.post)
     .put("/banco/actualizar/:cod_banco", BancoController.put)
     .delete("/banco/eliminar/:cod_banco", BancoController.delete)
-    .use(BancoController.error404)
-  //***CLIENTE RUTA***//
-    .get("/cliente/getall", ClienteController.getAll)
-    .post("/cliente/getone/:numero_cliente", ClienteController.getOne)
-    .post("/cliente/insertar", ClienteController.post)
-    .put("/cliente/actualizar/:numero_cliente", ClienteController.put)
-    .delete("/cliente/eliminar/:numero_cliente", ClienteController.delete)
-    .use(ClienteController.error404)
-  //***CUENTA RUTA***//
+    .use(BancoController.error404);
+
+
+var ClienteController = require("../controllers/cliente.controller"),
+    express = require("express"),
+    router = express.Router();
+    router
+      //***CLIENTE RUTA***//
+      .get("/cliente/getall", ClienteController.getAll)
+      .post("/cliente/getone/:numero_cliente", ClienteController.getOne)
+      .post("/cliente/insertar", ClienteController.post)
+      .put("/cliente/actualizar/:numero_cliente", ClienteController.put)
+      .delete("/cliente/eliminar/:numero_cliente", ClienteController.delete)
+      .use(ClienteController.error404);
+
+
+var CuentaController = require("../controllers/cuenta.controller"),
+    express = require("express"),
+    router = express.Router();
+    router
+    //***CUENTA RUTA***//
     .get("/cuenta/getall", ClienteController.getAll)
     .post("/cuenta/getone/:numero_cuenta", ClienteController.getOne)
     .post("/cuenta/insertar", ClienteController.post)
     .put("/cuenta/actualizar/:numero_cuenta", ClienteController.put)
     .delete("/cuenta/eliminar/:numero_cuenta", ClienteController.delete)
-    .use(ClienteController.error404);
+    .use(CuentaController.error404);
 
 module.exports = router;
